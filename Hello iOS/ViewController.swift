198538041255de27bca1ae23ba68568e8fc88a15
iwangy <35878784+iwangy@users.noreply.github.com>
2022-03-29 23:58:38 -0700

    Initial Commit

---
+++ Hello iOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hello iOS
+//
+//  Created by Ian Wang on 3/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let textLabel = UILabel();
+        textLabel.text = "Go Dawgs!"
+        textLabel.textAlignment = .center
+        textLabel.font = UIFont(name:"ArialRoundedMTBold", size: 35.0)
+        textLabel.frame = CGRect(x: view.frame.width / 2 - 100, y: view.frame.height / 2 - 50, width: 200, height: 200)
+        view.addSubview(textLabel)
+    }
+
+
+}
+
