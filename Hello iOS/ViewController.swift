346db261b26810d27de5a4f674c1983cb70de20f
iwangy <35878784+iwangy@users.noreply.github.com>
2022-04-04 21:54:24 -0700

    add screenshots, add storyboard view

--- Hello iOS/ViewController.swift
+++ Hello iOS/ViewController.swift
@@ -13,14 +13,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let textLabel = UILabel();
-        textLabel.text = "Go Dawgs!"
-        textLabel.textAlignment = .center
-        textLabel.font = UIFont(name:"ArialRoundedMTBold", size: 35.0)
-        textLabel.frame = CGRect(x: view.frame.width / 2 - 100, y: view.frame.height / 2 - 50, width: 200, height: 200)
-        view.addSubview(textLabel)
+//        let textLabel = UILabel();
+//        textLabel.contentMode = .scaleAspectFit
+//        textLabel.text = "Go Dawgs!"
+//        textLabel.textAlignment = .center
+//        textLabel.font = UIFont(name:"ArialRoundedMTBold", size: 35.0)
+//        textLabel.frame = CGRect(x: view.frame.width / 2 - 100, y: view.frame.height / 2 - 50, width: 200, height: 200)
+//        view.addSubview(textLabel)
     }
-
-
+    
 }
 
